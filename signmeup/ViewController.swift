bca55db557644f5aa9e8d8f7505b30ae266b5573
Nate Bird <natebird@gmail.com>
2015-09-18 08:24:56 -0400

    Initial Commit

---
+++ signmeup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  signmeup
+//
+//  Created by Nate Bird on 9/18/15.
+//  Copyright © 2015 NM Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
